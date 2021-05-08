a13599d15ae1769891e2c07e14b773fd55aa9ce7
czl0325 <>
2021-05-08 17:56:40 +0800

    首页长列表

--- bossjob-frontend/src/containers/main/home/index.jsx
+++ bossjob-frontend/src/containers/main/home/index.jsx
@@ -1,16 +1,20 @@
 import React, {Component} from 'react';
+import {NavBar, ListView, PullToRefresh} from "antd-mobile";
 import {getUserList} from "../../../api/api";
-import {NavBar} from "antd-mobile";
 
 export default class index extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      refreshing: true,
+      isLoading: true,
+      users: []
+    };
   }
 
   componentDidMount() {
     getUserList(1).then(res=>{
-
+      this.setState({users: res})
     })
   }
 
@@ -18,6 +22,20 @@ export default class index extends Component {
     return (
       <div>
         <NavBar>首页</NavBar>
+        <ListView
+          ref={el => this.listViewRef = el}
+          dataSource={this.state.users}
+          renderHeader={() => <span>下拉刷新</span>}
+          renderFooter={() => (<div style={{ padding: 30, textAlign: 'center' }}>
+            {this.state.isLoading ? '加载中' : '加载完成'}
+          </div>)}
+          pullToRefresh={<PullToRefresh
+            refreshing={this.state.refreshing}
+            onRefresh={this.onRefresh}
+          />}
+          onEndReached={this.onEndReached}
+          pageSize={5}
+        />
       </div>
     )
   }
